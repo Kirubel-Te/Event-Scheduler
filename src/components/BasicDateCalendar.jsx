6a239022f6ee12ee17577403ebacf607fb6b4cca
Kirubel Tesfaye <kirubeltesfaye519@gmail.com>
2025-05-18 23:56:46 +0300

    home page edit

---
+++ src/components/BasicDateCalendar.jsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+import { Box } from '@mui/material';
+
+export default function BasicDateCalendar() {
+    return (
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <Box
+                sx={{
+                    backgroundColor: '#000000',
+                    color: 'white',
+                    padding: 2,
+                    borderRadius: 2,
+                    display: 'inline-block',
+                }}
+            >
+                <DateCalendar
+                    sx={{
+                        backgroundColor: '#000000',
+                        color: 'white',
+                        '.MuiPickersCalendarHeader-root': {
+                        color: 'white',
+                        },
+                        '.MuiDayCalendar-weekDayLabel, .MuiDayCalendar-day': {
+                        color: 'white',
+                        },
+                        '.Mui-selected': {
+                            backgroundColor: '#212121', // Example selection color
+                        },
+                        '.MuiPickersDay-root': {
+                            color: 'white',             // 🟢 this sets the number (day) color
+                        },
+                    }}
+                />
+            </Box>
+        </LocalizationProvider>
+    );
+}
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import './Home.css'
 import Event from '../assets/event.svg'
 import Menu from '../assets/menuu.svg'
+import Icalendar from './BasicDateCalendar'
 
 export default function Home(){
     return(
@@ -12,6 +13,12 @@ export default function Home(){
                 <h1>Evnet Scheduler</h1>
                 <img src={Event}/>
             </header>
+            <div className='cal-home'>
+                <div>
+                    <p>Effortlessly manage your schedule and collaborate in real-time</p>
+                </div>
+                <Icalendar/>
+            </div>
         </div>
     )
 }
